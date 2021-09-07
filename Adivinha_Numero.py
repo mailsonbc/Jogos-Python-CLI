f5f938e7853dfe3db99fa8180b9f04d2e629d6b2
Mailson Barreto <mailsonbc@gmail.com>
2021-09-07 15:35:55 -0300

    Adicionado loop para saber se o jogador quer continuar o jogo errando ou acertando.

--- Adivinha_Numero.py
+++ Adivinha_Numero.py
@@ -1,16 +1,19 @@
 import random
 
-numero = random.randrange(0, 10)
 
-print("Tente adivinhar o número secreto." )
-adivinha = int(input("Digite um número entre 0 e 10: "))
+continua = ""
+
+while (continua != ("n" or "N")):
+    numero = random.randrange(0, 10)
+
+    print("Tente adivinhar o número secreto." )
+    adivinha = int(input("Digite um número entre 0 e 10: "))
 
 
-if  (adivinha == numero):
-    print("Acertou!!!!")
-else:
-    print("Errou. O número é: %d " %numero)
-    print("Perdeu!!!")
-
-print("Aperte qualquer tecla para encerrar: ")
-input()
+    if  (adivinha == numero):
+        print("Acertou!!!!")
+    else:
+        print("Errou. O número é: %d " %numero)
+        print("Perdeu!!!")
+    
+    continua = input("Quer continuar? Sim(s) ou Não(n): ")
